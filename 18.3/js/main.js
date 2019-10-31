05cdfeb491392d0cfda80240efcd5db5bf2ca123
David Chen <davidchen1994520@gmail.com>
2019-10-31 12:49:30 +0800

    vuejs test18.3

---
+++ 18.3/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+      url:'https://v1-cn.vuejs.org/guide/conditional.html',
+      img:'https://dummyimage.com/100x100/000/fff' ,
+      klass:'btn btn-default',
+      isActive: true,
+    }
+});

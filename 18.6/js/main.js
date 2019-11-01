dbae02b117382471b46cb766de22d28654bfb875
David Chen <davidchen1994520@gmail.com>
2019-11-01 12:16:29 +0800

    vuejs test18.6

---
+++ 18.6/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+      from: 1,
+      dest: [], 
+      // sex:['male']
+      // article: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. A voluptatum recusandae, corporis atque labore voluptate aliquid vero! Neque, consectetur itaque nam ab eligendi tenetur dolorum culpa, mollitia, alias accusamus enim!',
+    }  
+});
